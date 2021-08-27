97e98a58ac78a669ea0f11a3d8a9823404fcae4e
Vyacheslav Volkhin <vv@htmlworker.com>
2021-08-27 16:17:05 +0500

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,5 @@
 $(document).ready(function(){
-
-	//phone masked
-	$('input[type="tel"]').mask("+38 (999) 999-99-99",{placeholder:"+38 (___) ___-__-__"});
-	$('input[type="tel"]').on('click', function() {
-		$(this).setCursorPosition(4);
-	})
-	$.fn.setCursorPosition = function(pos) {
-	  this.each(function(index, elem) {
-	    if (elem.setSelectionRange) {
-	      elem.setSelectionRange(pos, pos);
-	    } else if (elem.createTextRange) {
-	      var range = elem.createTextRange();
-	      range.collapse(true);
-	      range.moveEnd('character', pos);
-	      range.moveStart('character', pos);
-	      range.select();
-	    }
-	  });
-	  return this;
-	};
+    
 
 	//popup block
 	$('.js-popup-wrap .js-btn-toggle').on('click', function() {
